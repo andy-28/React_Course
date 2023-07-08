207a537e9c5301d463d52e4302654d62f4e9d68b
andy-28 <a5706617@gmail.com>
2023-07-08 15:25:09 +0800

    commit

--- e-lint/src/App.js
+++ e-lint/src/App.js
@@ -24,7 +24,9 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -56,7 +58,42 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🧝🏻‍♀️ Friend</label>
+      <input type="text" />
+      <label>🧝🏻‍♀️ Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>💰 Bill value</label>
+      <input type="text" />
+      <label>🧍‍♀️ Your expense</label>
+      <input type="text" />
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
   );
 }
