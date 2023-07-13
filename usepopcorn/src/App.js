755d26b70b07e2cf0e8e2b1e79437a594b683db1
andy-28 <a5706617@gmail.com>
2023-07-13 10:02:22 +0800

    commit

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -50,13 +50,13 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "491d9e55";
+const KEY = "87660ae2";
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
 
-  fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=interstellar`)
+  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
     .then((res) => res.json())
     .then((data) => console.log(data));
 
