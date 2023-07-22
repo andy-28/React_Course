05ca1a48bc18ff9b27c0f21a218177b4336e4960
andy-28 <a5706617@gmail.com>
2023-07-22 17:28:51 +0800

    commit

---
+++ react-quiz/src/components/Progress.js
@@ -0,0 +1,12 @@
+function Progress({ index, numQuestion }) {
+  return (
+    <header className="progress">
+      <p>
+        Question
+        <strong>{index}</strong> / {numQuestion}
+      </p>
+    </header>
+  );
+}
+
+export default Progress;
