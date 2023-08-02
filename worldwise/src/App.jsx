8ecc3ed21eb8885ee2f67e76ed45ea7530123c2b
andy-28 <a5706617@gmail.com>
2023-08-02 15:25:39 +0800

    commit

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+
 function App() {
-  return <div>worldwise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/product" element={<Product />}></Route>
+        <Route path="/pricing" element={<Pricing />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
