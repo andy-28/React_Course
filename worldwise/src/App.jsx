e33b766e93be5cc2cb6492a11005de7c6b428dc4
andy-28 <a5706617@gmail.com>
2023-08-02 15:26:56 +0800

    commit

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/product" element={<Product />}></Route>
-        <Route path="/pricing" element={<Pricing />}></Route>
+        <Route path="product" element={<Product />}></Route>
+        <Route path="pricing" element={<Pricing />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </BrowserRouter>
