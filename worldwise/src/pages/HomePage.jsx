7d7a2a138fdac02115773acd41d0a0a1fd076388
andy-28 <a5706617@gmail.com>
2023-08-04 15:21:50 +0800

    commit

--- worldwise/src/pages/HomePage.jsx
+++ worldwise/src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
   return (
     <div>
       <h1>HomePage</h1>
 
       <a href="/pricing">Pricing</a>
+      <Link to="/pricing">Pricing</Link>
     </div>
   );
 }
